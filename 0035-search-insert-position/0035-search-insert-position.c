581307371bf2215309db9a38121bdd1fa835d1a8
Prashant Gupta <guptaprashant9589@gmail.com>
2026-07-15 19:22:23 +0530

    Time: 0 ms (100%), Space: 8.9 MB (70.25%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,21 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int beg=0,end=numsSize-1,loc=-1;
+    while(beg<=end){
+      int  mid=(beg+end)/2;
+        if(target==nums[mid]){
+            loc=mid;
+            break;
+        }
+        else if(target<nums[mid]) {
+            end=mid-1;
+        }
+        else {
+            beg=mid+1;
+        }}
+        if(loc==-1){
+            numsSize++;
+            loc=beg;
+        }
+    
+    return loc;
+}
